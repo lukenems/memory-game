376d93065becd8e7053e550d4715b30c1a61768f
Luke Nemy <lukenemy7@gmail.com>
2019-07-24 13:45:35 -0700

    card flipping tweeks and matching cards functionality set up!

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
 const cards = document.querySelectorAll('.card');
-console.log(cards)
 
+let firstCardFlipped = false;
+let firstCard, secondCard;
 
 function flipCard(){
     this.classList.toggle('flip')
+
+    if(!firstCardFlipped){
+        //first click
+        firstCardFlipped = true;
+        firstCard = this;
+        return
+    } 
+    //second click
+    firstCardFlipped = false;
+    secondCard = this;
+    checkForMatch()
+
+}
+
+function checkForMatch() {
+     //do cards match?
+     if(firstCard.dataset.glyph === secondCard.dataset.glyph){
+        matchingPair()
+    } else {
+        resetCards()
+    }
+}
+
+function matchingPair() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function resetCards() {
+    setTimeout(function(){
+        firstCard.classList.remove('flip')
+        secondCard.classList.remove('flip')
+    }, 1250)
 }
 
 cards.forEach(function(card){
